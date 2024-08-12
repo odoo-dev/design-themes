809a152ae1f0214f2c3a92757c8471ebf0f152da
Antoine (anso) <anso@odoo.com>
2024-08-12 11:57:54 +0200

    [IMP] theme_bookstore: theme improvement
    Revamp of `theme_bookstore`
    
    task-4109584

--- theme_bookstore/static/src/js/tour.js
+++ theme_bookstore/static/src/js/tour.js
@@ -15,29 +15,29 @@ const snippets = [
         groupName: "Images",
     },
     {
-        id: 's_image_text',
-        name: 'Image - Text',
+        id: 's_process_steps',
+        name: 'Steps',
         groupName: "Content",
     },
     {
-        id: 's_picture',
-        name: 'Title - Image',
+        id: 's_showcase',
+        name: 'Showcase',
+        groupName: "Content",
+    },
+    {
+        id: 's_title',
+        name: 'Title',
+        groupName: "Text",
+    },
+    {
+        id: 's_images_wall',
+        name: 'Images Wall',
         groupName: "Images",
-    },
-    {
-        id: 's_product_list',
-        name: 'Items',
-        groupName: "Content",
-    },
-    {
-        id: 's_call_to_action',
-        name: 'Call to Action',
-        groupName: "Content",
     },
 ];
 
 wTourUtils.registerThemeHomepageTour("bookstore_tour", () => [
-    wTourUtils.assertCssVariable('--color-palettes-name', '"generic-8"'),
+    wTourUtils.assertCssVariable('--color-palettes-name', '"default-26"'),
     ...wTourUtils.dragNDrop(snippets[0]),
     ...wTourUtils.clickOnText(snippets[0], 'h1'),
     wTourUtils.goBackToBlocks(),
