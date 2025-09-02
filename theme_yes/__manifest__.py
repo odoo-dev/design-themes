f209ed1dce66de839ff43f8b2185c3ccc9081975
Xavier Luyckx (xlu) <xlu@odoo.com>
2025-09-02 14:31:50 +0200

    [IMP] theme_yes: add and customize wsale categories snippet
    This commit includes and customize the
    `website_sale.s_dynamic_snippet_category_list` snippet in the default
    snippet sequence of the theme's homepage.
    
    task-5009694

--- theme_yes/__manifest__.py
+++ theme_yes/__manifest__.py
@@ -91,6 +91,35 @@
     'configurator_snippets': {
         'homepage': ['s_kickoff', 's_title', 's_company_team', 's_image_text_overlap', 's_features', 's_freegrid', 's_quotes_carousel', 's_call_to_action'],
     },
+    'configurator_snippets_addons': {
+        'website_sale': {
+            'homepage': [
+                ('website_sale.s_dynamic_snippet_category_list', 'after', 's_freegrid'),
+            ],
+        },
+    },
+    'theme_customizations': {
+        'website_sale.s_dynamic_snippet_category_list': {
+            'data_attributes': {
+                'columns': '2',
+                'gap': '3',
+                'size': 'small',
+                'alignment': 'left',
+            },
+            'background': {
+                'color': 'o_cc3',
+            },
+            'add_classes': [
+                'pb88', 'pt88',
+                {
+                    's_dynamic_snippet_title': 's_dynamic_snippet_title_aside col-lg-3 flex-lg-column justify-content-lg-start'
+                },
+            ],
+            'remove_classes': [
+                'pb64', 'pt64',
+            ],
+        },
+    },
     'new_page_templates': {
         'about': {
             'personal': ['s_text_cover', 's_image_text', 's_text_block_h2', 's_numbers', 's_features', 's_call_to_action'],
